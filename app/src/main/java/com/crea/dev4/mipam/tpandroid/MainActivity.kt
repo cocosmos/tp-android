3b5b0fd3d7431b2eb44b63804382737a86c77067
Cocosmos <mipam.guillot@crea-inseec.com>
2022-12-05 18:37:03 +0100

    Add latitude and longitude via geolocalisation

--- app/src/main/java/com/crea/dev4/mipam/tpandroid/MainActivity.kt
+++ app/src/main/java/com/crea/dev4/mipam/tpandroid/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.crea.dev4.mipam.tpandroid
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.*
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.mapSaver
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.crea.dev4.mipam.tpandroid.ui.theme.TPAndroidTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+     private var tvLatitude: String by mutableStateOf("")
+     private var tvLongitude: String by mutableStateOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,175 +31,131 @@ class MainActivity : ComponentActivity() {
                 MyApp()
             }
         }
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getCurrentLocation()
     }
-}
 
 
-data class Answer(var name: String, var happy: String)
 
-val AnswerSaver = run {
-    val nameKey = "Name"
-    val happyKey = "Happy"
-    mapSaver(
-        save = { mapOf(nameKey to it.name, happyKey to it.happy) },
-        restore = { Answer(it[nameKey] as String, it[happyKey] as String) }
-    )
-}
-
-@Composable
-private fun MyApp() {
-    var shouldShowBoarding by rememberSaveable { mutableStateOf(true) }
-    val shouldGetFormData by rememberSaveable(stateSaver = AnswerSaver) {
-        mutableStateOf(
-            Answer(
-                "",
-                ""
+    /**
+     * The App function is the entry point of the app.
+     */
+    @Composable
+    private fun MyApp() {
+        var shouldShowBoarding by rememberSaveable { mutableStateOf(true) }
+        val shouldGetFormData by rememberSaveable(stateSaver = answerSaver) {
+            mutableStateOf(
+                Answer(
+                    "",
+                    "",
+                    "",
+                    ""
+                )
             )
-        )
-    }
+        }
 
-    if (shouldShowBoarding) {
-        BoardingScreen(
-            onDismiss = { shouldShowBoarding = false }, shouldGetFormData = shouldGetFormData,
-        )
-    } else {
-        FormScreen(
-            onDismiss = { shouldShowBoarding = true },
-            shouldGetFormData = shouldGetFormData
-        )
-    }
-}
+        if (shouldShowBoarding) {
+            BoardingScreen(
+                onDismiss = { shouldShowBoarding = false }, shouldGetFormData = shouldGetFormData,
+            )
+        } else {
+            FormScreen(
+                onDismiss = { shouldShowBoarding = true },
+                shouldGetFormData = shouldGetFormData,
+                tvLongitude = tvLongitude,
+                tvLatitude = tvLatitude,
+                getCurrentLocation = { getCurrentLocation() }
 
-@Composable
-fun BoardingScreen(
-    onDismiss: () -> Unit,
-    shouldGetFormData: Answer,
-) {
-
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Bienvenue sur MipamApp!", style = MaterialTheme.typography.h5)
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onDismiss
-            ) {
-                Text("Remplir formulaire")
-            }
-
-            ShowAnswers(shouldGetFormData)
+            )
         }
     }
-}
 
-@Composable
-fun ShowAnswers(answers: Answer) {
-
-    Column {
-        Text("Réponse formulaire:", style = MaterialTheme.typography.h6)
-        Text("Nom: ${answers.name}")
-        Text("Heureux: ${answers.happy}")
-    }
-
-}
-
-
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    heightDp = 320,
-    name = "DefaultPreview",
-    uiMode = UI_MODE_NIGHT_YES
-)
-@Composable
-fun DefaultPreview() {
-    TPAndroidTheme {
-        MyApp()
-    }
-}
-
-
-@Composable
-private fun FormScreen(onDismiss: () -> Unit, shouldGetFormData: Answer) {
-    val radioOptions = listOf("Oui", "Peut-être", "Non")
-
-    val (name, setName) = rememberSaveable { mutableStateOf("") }
-    val (selectedOption, onOptionSelected) = rememberSaveable { mutableStateOf(radioOptions[0]) }
-
-    fun onContinueClicked() {
-        shouldGetFormData.name = name
-        shouldGetFormData.happy = selectedOption
-        onDismiss()
-    }
-
-
-    Surface {
-        Column(
-            modifier = Modifier
-                .padding(16.dp),
-
-            ) {
-            TextField(
-                value = name,
-                onValueChange = { setName(it) },
-                label = { Text("Votre nom") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp)
-
-            )
-            Text("Es-tu heureux ?")
-            Column {
-                for (radioOption in radioOptions) {
-                    Row(modifier = Modifier
-                        .fillMaxWidth()
-                        .selectable(radioOption == selectedOption, onClick = {
-                            onOptionSelected(radioOption)
-                        })
-                    ) {
-                        RadioButton(selected = radioOption == selectedOption, onClick = {
-                            onOptionSelected(radioOption)
-                        })
-                        Text(text = radioOption, modifier = Modifier.padding(top = 13.dp))
+    /**
+     * This function is used to get the current location of the device.
+     */
+    @SuppressLint("MissingPermission") // We check for permission before calling the method
+     private fun getCurrentLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location == null) {
+                        requestPermissions()
+                        Toast.makeText(this, "No last location", Toast.LENGTH_LONG).show()
+                    } else {
+                        tvLongitude = location.longitude.toString()
+                        tvLatitude = location.latitude.toString()
                     }
                 }
 
+            } else {
+                Toast.makeText(this, "Please turn on your location...", Toast.LENGTH_LONG)
+                    .show()
             }
-
-
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp),
-                onClick = { onContinueClicked() }
-            ) {
-                Text("Valider")
-            }
+        } else {
+            requestPermissions()
         }
     }
 
+    /**
+     * Check if location is enabled
+     */
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+    /**
+     * Check if the app has the necessary permissions to get the user's location
+     */
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+    /**
+     * Request the necessary permissions to get the user's location
+     */
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            100
+        )
+    }
+
+    /**
+     * This function is used to save the state of the form.
+     */
+    data class Answer(
+        var name: String,
+        var happy: String,
+        var latitude: String,
+        var longitude: String
+    )
+    private val answerSaver = run {
+        val nameKey = "Name"
+        val happyKey = "Happy"
+        val latitudeKey = "Latitude"
+        val longitudeKey = "Longitude"
+        mapSaver(
+            save = { mapOf(nameKey to it.name, happyKey to it.happy, latitudeKey to it.latitude, longitudeKey to it.longitude) },
+            restore = { Answer(it[nameKey] as String, it[happyKey] as String, it[latitudeKey] as String, it[longitudeKey] as String ) }
+        )
+    }
+
 }
-
-
-//@Preview(showBackground = true, widthDp = 360, heightDp = 640,
-//    name = "DefaultPreview")
-//@Composable
-//private fun DefaultPreview() {
-//    TPAndroidTheme {
-//        Form()
-//    }
-//}
-//
-//
-//
-//@Preview(showBackground = true, widthDp = 360, heightDp = 640,uiMode = UI_MODE_NIGHT_YES,
-//    name = "DefaultPreviewDark")
-//@Composable
-//private fun DefaultPreviewDark() {
-//    TPAndroidTheme {
-//      Form()
-//    }
-//}
